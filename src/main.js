e09bb61b309d3608766c3d3a9e589949f2e50d5c
ZhenJie <cn.zhenjie@gmail.com>
2019-12-06 12:00:06 +0800

    install iview plugin

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ViewUI from 'view-design'
+import './plugins/iview.js'
 
 Vue.config.productionTip = false
+Vue.use(ViewUI)
 
 new Vue({
   router,
